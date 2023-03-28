73109f23c4bed8e19b3a4a864e8ab230e8e3305f
jaka2005 <jakalogin1808@gmail.com>
2023-03-28 19:47:28 +0400

    created EventListener

---
+++ src/nativeMain/kotlin/event.kt
@@ -0,0 +1,26 @@
+sealed class Event
+
+interface EventType<T : Event>
+
+fun interface EventHandler<T: Event> {
+    operator fun invoke(event: T)
+}
+
+interface IEventListener {
+    fun <T : Event> onEvent(eventType: EventType<T>, handler: EventHandler<T>)
+    fun <T : Event> dispatch(eventType: EventType<T>, event: T)
+}
+
+open class EventListener : IEventListener {
+    protected val eventListeners =
+        mutableMapOf<EventType<*>, MutableList<EventHandler<*>>>()
+
+    override fun <T : Event> onEvent(eventType: EventType<T>, handler: EventHandler<T>) {
+        eventListeners.getOrPut(eventType) { mutableListOf() }.add(handler)
+    }
+    override fun <T : Event> dispatch(eventType: EventType<T>, event: T) {
+        eventListeners[eventType]?.forEach { handler ->
+            (handler as EventHandler<T>).invoke(event)
+        }
+    }
+}
